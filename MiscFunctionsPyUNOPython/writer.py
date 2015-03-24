9ebc815e7af4d6c9ddf05769df3e6332886399ae
Nikita Uvarov <uv.nikita@gmail.com>
2015-03-24 18:09:13 +0200

    tdf#64900: Refactor and extend pyUNO examples (Writer)
    Change-Id: Ia0759c5ab60b8a8603e0d6675bc75d9f91c8250e
    Reviewed-on: https://gerrit.libreoffice.org/14988
    Reviewed-by: Björn Michaelsen <bjoern.michaelsen@canonical.com>
    Tested-by: Björn Michaelsen <bjoern.michaelsen@canonical.com>

--- MiscFunctionsPyUNOPython/writer.py
+++ MiscFunctionsPyUNOPython/writer.py
@@ -9,186 +9,17 @@ __author__ = 'mlechner'
 #   http://www.gnu.org/licenses/lgpl-2.1.html
 #
 
-import sys
-import atexit
 import os
-import time
-import uno
 import unohelper
 from com.sun.star.beans import PropertyValue
-from com.sun.star.connection import NoConnectException
 from com.sun.star.awt import Size
 from com.sun.star.text.ControlCharacter import PARAGRAPH_BREAK, LINE_BREAK
-from com.sun.star.style.BreakType import PAGE_BEFORE, PAGE_AFTER
-from com.sun.star.style.ParagraphAdjust import CENTER, LEFT, RIGHT
 from com.sun.star.awt.FontWeight import BOLD as FW_BOLD
-from com.sun.star.awt.FontSlant import ITALIC as FW_ITALIC
-from com.sun.star.awt.FontWeight import NORMAL as FW_NORMAL
 
-OPENOFFICE_PORT = 2002
-_oopaths=(
-    ('/usr/lib/libreoffice/program/', '/usr/lib/libreoffice/program/'),
-    ('/usr/lib64/libreoffice/program/', '/usr/lib64/libreoffice/program/'),
-)
-
-for p in _oopaths:
-    if os.path.exists(p[0]):
-        OPENOFFICE_PATH    = p[0]
-        OPENOFFICE_BIN     = os.path.join(OPENOFFICE_PATH, 'soffice')
-        OPENOFFICE_LIBPATH = p[1]
-
-        # Add to path so we can find uno.
-        if sys.path.count(OPENOFFICE_LIBPATH) == 0:
-            sys.path.insert(0, OPENOFFICE_LIBPATH)
-        break
-
-class OORunner:
-    """
-    Start, stop, and connect to OpenOffice.
-    """
-    def __init__(self, port=OPENOFFICE_PORT):
-        """ Create OORunner that connects on the specified port. """
-        self.port = port
-
-    def connect(self, no_startup=False):
-        """
-        Connect to OpenOffice.
-        If a connection cannot be established try to start OpenOffice.
-        """
-        localContext = uno.getComponentContext()
-        servicemanager = localContext.ServiceManager
-        resolver = servicemanager.createInstanceWithContext("com.sun.star.bridge.UnoUrlResolver", localContext)
-        context = None
-        did_start = False
-
-        n = 0
-        while n < 6:
-            try:
-                context = resolver.resolve("uno:socket,host=localhost,port=%d;urp;StarOffice.ComponentContext" % self.port)
-                break
-            except NoConnectException:
-                pass
-
-            # If first connect failed then try starting OpenOffice.
-            if n == 0:
-                # Exit loop if startup not desired.
-                if no_startup:
-                     break
-                self.startup()
-                did_start = True
-
-            # Pause and try again to connect
-            time.sleep(1)
-            n += 1
-
-        if not context:
-            raise Exception("Failed to connect to OpenOffice on port %d" % self.port)
-
-        desktop = context.ServiceManager.createInstanceWithContext("com.sun.star.frame.Desktop", context)
-        # generate a graphic provider as well to embed images from filesystem
-        graphicprovider = context.ServiceManager.createInstance('com.sun.star.graphic.GraphicProvider')
-
-        if not (desktop and graphicprovider):
-            raise Exception("Failed to create OpenOffice desktop or graphicprovider on port %d" % self.port)
-
-        if did_start:
-            _started_desktops[self.port] = desktop
-            _started_graphicproviders[self.port] = graphicprovider
-
-        return desktop, graphicprovider
-
-    def startup(self):
-        """
-        Start a headless instance of OpenOffice.
-        """
-        args = [OPENOFFICE_BIN,
-                '--headless',
-                '--accept=socket,host=localhost,port=%d;urp;StarOffice.ServiceManager;tcpNoDelay=1;' % self.port,
-                '--nologo',
-                '--invisible',
-                '--nofirststartwizard',
-                '--nocrashreport',
-                '--norestore'
-                ]
-        env  = {'PATH'       : '/bin:/usr/bin:%s' % OPENOFFICE_PATH,
-                'PYTHONPATH' : OPENOFFICE_LIBPATH,
-                }
-
-        try:
-            pid = os.spawnve(os.P_NOWAIT, args[0], args, env)
-        except Exception as e:
-            raiseException("Failed to start OpenOffice on port %d: %s" % (self.port, e.message))
-
-        if pid <= 0:
-            raise Exception("Failed to start OpenOffice on port %d" % self.port)
-
-    def shutdown(self):
-        """
-        Shutdown OpenOffice.
-        """
-        try:
-            if _started_desktops.get(self.port):
-                _started_desktops[self.port].terminate()
-                del _started_desktops[self.port]
-            if _started_graphicproviders.get(self.port):
-                _started_graphicproviders[self.port].terminate()
-                del _started_graphicproviders[self.port]
-        except Exception as e:
-            pass
-
-# Keep track of started desktops and shut them down on exit.
-_started_desktops = {}
-_started_graphicproviders = {}
-
-def _shutdown_desktops():
-    """ Shutdown all OpenOffice desktops that were started by the program. """
-    for port, desktop in _started_desktops.items():
-        try:
-            if desktop:
-                desktop.terminate()
-        except Exception as e:
-            pass
-
-def _shutdown_graphicproviders():
-    """ Shutdown all OpenOffice graphicproviders that were started by the program. """
-    for port, graphicprovider in _started_graphicproviders.items():
-        try:
-            if graphicprovider:
-                graphicprovider.terminate()
-        except Exception as e:
-            pass
+from runner import OORunner
 
 
-atexit.register(_shutdown_desktops)
-atexit.register(_shutdown_graphicproviders)
-
-
-def oo_shutdown_if_running(port=OPENOFFICE_PORT):
-    """ Shutdown OpenOffice if it's running on the specified port. """
-    oorunner = OORunner(port)
-    try:
-        desktop, graphicprovider = oorunner.connect(no_startup=True)
-        desktop.terminate()
-        graphicprovider.terminate()
-    except Exception as e:
-        pass
-
-
-def oo_properties(**args):
-    """
-    Convert args to OpenOffice property values.
-    """
-    props = []
-    for key in args:
-        prop = PropertyValue()
-        prop.Name = key
-        prop.Value = args[key]
-        props.append(prop)
-
-    return tuple(props)
-
-# Examples here
-class WriterExamples:
+class WriterExamples(object):
     def run(self):
         oor = OORunner()
         self.desktop, self.graphicprovider = oor.connect()
@@ -200,8 +31,9 @@ class WriterExamples:
         self.cl_ID = 0
         # do what ever you like from the following functions
         self.do_heading('First Heading')
-        self.do_paragraph('Lorem ipsum')
+        self.do_paragraph('Lorem ipsum', 14)
         # ... go on ...
+        oor.shutdown()
 
     # search and replace text
     def do_search_and_replace(self, searchtext=None, replacetext=None):
@@ -217,12 +49,10 @@ class WriterExamples:
 
     def add_paragraph_break(self):
         doc = self.edit_doc
-        cursor = doc.Text.createTextCursor()
         doc.Text.insertControlCharacter(self.cursor, PARAGRAPH_BREAK, 0)
 
     def add_line_break(self):
         doc = self.edit_doc
-        cursor = doc.Text.createTextCursor()
         doc.Text.insertControlCharacter(self.cursor, LINE_BREAK, 0)
 
     # add a pagebreak
@@ -232,7 +62,7 @@ class WriterExamples:
         cursor = doc.Text.createTextCursor()
         cursor.gotoEnd(False)
         doc.Text.insertControlCharacter(self.cursor, 4, 0)
-        cursor.BreakType = 4 
+        cursor.BreakType = 4
         if vorlage:
             try:
                 cursor.setPropertyValue("PageDescName", vorlage)
@@ -386,7 +216,7 @@ class WriterExamples:
             table.getCellByName(k).setString(k)
             table.getCellByName(v).setString(v)
 
-# Main
+
 if __name__ == '__main__':
     writerexamples = WriterExamples()
     writerexamples.run()
